43b6eede8523308d336f0830e6f0895081b513bf
maskl <masklod@gmail.com>
2011-12-27 13:02:17 +0100

    Example MVVM Light Project.

---
+++ Clime/Clime/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Clime.Model;
+
+namespace Clime.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
